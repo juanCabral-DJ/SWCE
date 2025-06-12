e01a3c8bdc631ea5eb0c6bfe80b1850092bd5ec5
juanCabral-DJ <juandavidcabral71@gmail.com>
2025-06-11 21:21:05 -0700

    Elimina archivos antiguos y agrega nueva estructura de Entities

--- SWCE.Domain/Entities/Configuration/WishListItem.cs
+++ SWCE.Domain/Entities/Configuration/WishListItem.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace SWCE.Domain
+namespace SWCE.Domain.Entities.Configuration
 {
     internal class WishListItem
     {
