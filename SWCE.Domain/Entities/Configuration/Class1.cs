fab3298673f2e8c1008119fa36a51914719b31a1
juanCabral-DJ <juandavidcabral71@gmail.com>
2025-06-11 21:52:48 -0700

    prueba

---
+++ SWCE.Domain/Entities/Configuration/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SWCE.Domain.Entities.Configuration
+{
+    internal class Class1
+    {
+    }
+}
