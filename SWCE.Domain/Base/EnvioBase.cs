db2c8578c92c03b34a82b4cde6b4f0a5d26d0dda
Jeferson Castillo <jefersonmanuelcastillolara@gmail.com>
2025-06-12 23:42:54 -0400

    Creacion clases modulo envio

---
+++ SWCE.Domain/Base/EnvioBase.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SWCE.Domain.Base
+{
+    abstract class EnvioBase
+    {
+        public Pedido pedido {  get; set; }
+        public Direccion DireccionDestino { get; set; }
+        public string Estado {  get; set; }
+
+        public EnvioBase(Pedido pedido, Direccion direccion) 
+        {
+            this.pedido = pedido;
+            this.DireccionDestino = direccion;
+            this.Estado = "Pendiente";
+        }
+
+        public abstract decimal CalcularCosto();
+        public abstract string GenerarGuia();
+    }
+}
---
+++ SWCE.Domain/Entities/EnvioTerrestre.cs
@@ -0,0 +1,26 @@
+﻿using SWCE.Domain.Base;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SWCE.Domain.Entities
+{
+    public class EnvioTerrestre : EnvioBase
+    {
+        public EnvioTerrestre(Pedido pedido, Direccion direccion) : base(pedido, direccion)
+        {
+        }
+
+        public override decimal CalcularCosto()
+        {
+            return 50.0m;
+        }
+
+        public override string GenerarGuia()
+        {
+            return "GT10322";
+        }
+    }
+}
