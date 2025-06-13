ccff2caf51a3ac75499aef9abd18fe04ceb08395
Joel <joelguerrae1405@gmail.com>
2025-06-12 23:25:41 -0400

    Creación Capa de Dominio del Modulo de Carrito

---
+++ SWCE.Domain/Entities/Configuration/Carrito.cs
@@ -0,0 +1,74 @@
+﻿using SWCE.Domain.Repository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SWCE.Domain.Entities.Configuration
+{
+    internal class Carrito : ICarrito
+    {
+        private readonly List<ItemCarrito> productos = new();
+        private decimal total;
+
+        public int IdCarrito{ get; set; }
+        public int IdUsuario{ get; set; }
+        public decimal SubTotal { get; set; }
+        public decimal Descuento { get; set; }
+        public decimal Total { get; set; }
+
+        public Carrito(int IdUsuario) {
+            this.IdUsuario= IdUsuario;
+        }
+
+        public void AgregarItem (ProductoMock p, int cantidad)
+        {
+            if (p == null) throw new InvalidOperationException("Producto inválido");
+            if (cantidad <= 0) throw new InvalidOperationException("Cantidad inválida");
+
+            var existeProducto = productos.FirstOrDefault(i => i.IdProducto == p.Id);
+
+            if (existeProducto != null)
+            {
+                existeProducto.incrementarCantidad(cantidad);
+            }
+            else
+            {
+                productos.Add(new ItemCarrito(p, cantidad));
+            }
+
+            this.SubTotal = CalcularSubTotal();
+        }
+        
+        public void EliminarItem (int IdProducto)
+        {
+            var producto = productos.FirstOrDefault(i => i.IdProducto == IdProducto);
+            
+            if (producto != null)
+            {
+                productos.Remove(producto);
+            }
+        }
+
+        public decimal CalcularSubTotal()
+        {
+            decimal subt = 0m;
+
+            foreach (var item in productos)
+            {
+                subt = + item.SubTotal;
+            }
+
+            return subt;
+        }
+
+        // Falta implementar mejor el apartado de cupones para verificar como hacer los descuentos
+        public decimal CalcularTotal()
+        {
+            return total = SubTotal - Descuento;
+
+        }
+
+    }
+}
---
+++ SWCE.Domain/Entities/Configuration/ItemCarrito.cs
@@ -0,0 +1,53 @@
+﻿using SWCE.Domain.Repository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SWCE.Domain.Entities.Configuration
+{
+    internal class ItemCarrito : IItemCarrito
+    {
+        // Atributos
+        public int IdProducto { get; private set; }
+        public string NombreProducto { get; private set; }
+        public decimal PrecioUnitario { get; private set; }
+        public int Cantidad { get; private set; }
+        public decimal SubTotal { get; private set; }
+
+        // Constructor
+        public ItemCarrito(ProductoMock p, int cantidad)
+        {
+            if (p == null) throw new InvalidOperationException("Producto inválido");
+            if (cantidad <= 0) throw new InvalidOperationException("Cantidad inválida");
+
+            IdProducto = p.Id;
+            NombreProducto = p.Nombre;
+            PrecioUnitario = p.Precio;
+            Cantidad = cantidad;
+            SubTotal = calcularSubTotal();
+        }
+
+        public void incrementarCantidad(int cantidad)
+        {
+            if (cantidad <= 0)
+            {
+                throw new InvalidOperationException("Cantidad inválida");
+            }
+            this.Cantidad =+ cantidad;
+            calcularSubTotal();
+        }
+        public void actualizarCantidad(int nuevaCantidad)
+        {
+            if (nuevaCantidad <= 0) throw new InvalidOperationException("Cantidad Invalida");
+            this.Cantidad = nuevaCantidad;
+            calcularSubTotal();
+        }
+
+        public decimal calcularSubTotal()
+        {
+            return PrecioUnitario * Cantidad;
+        }
+    }
+}
