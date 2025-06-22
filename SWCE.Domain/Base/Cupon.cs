1eebb26f8bfb3b36ae088856de59ae63b7a2d297
JoaquinErnestoMeloNunez <joamelonunez@gmail.com>
2025-06-21 23:03:09 -0400

    Corrección de errores con una entidad.

---
+++ SWCE.Domain/Base/Cupon.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SWCE.Domain.Base
+{
+    public abstract class Cupon
+    {
+        protected string? Codigo { get; set; }
+        protected DateTime FechaExpiracion { get; set; }
+
+        public bool EstaVigente() => FechaExpiracion >= DateTime.Today;
+
+        public abstract decimal CalcularDescuento(decimal monto);
+    }
+}
